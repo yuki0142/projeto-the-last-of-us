5a31ebf271e792522bb1ab62b41d6c5a1140ab67
Adriel da Costa <127637336+yuki0142@users.noreply.github.com>
2023-03-11 22:45:31 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+//- passo 1 - dar um jeito de pegar o elemento HTML dos botões
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+
+//- passo 2 - dar um jeito de identificar o clique do usuário no botão
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+
+        desativarBotaoSelecionado();
+
+        selecionarBotaoCarrossel(botao);
+
+        esconderImagemAtiva();
+
+        mostrarImagemDeFundo(indice);
+    })
+})
+
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa');
+}
+
+function selecionarBotaoCarrossel(botao) {
+    botao.classList.add('selecionado');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
